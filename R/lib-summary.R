5e483f3d28cf3f639e8fa0a5d5a95eea758d5ea0
greenhorn82 <robert.busching@gmx.net>
2023-09-17 16:13:11 -0500

    * added size parameter * added documentation

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -3,16 +3,30 @@
 #' This functions generate a list of all installed packages, sorted by
 #' individual folder. The main target is to install.
 #'
+#' @param sizes logical indicating whether or not calculate the directory size
+#'
 #' @return a `data.frame` containing two columns. One for the library and one
 #'    for the number of packages.
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function(){
+lib_summary <- function(sizes = FALSE){
+  if(!is.logical(sizes)){
+    stop("'sizes' must be TRUE or FALSE")
+  }
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if(sizes){
+    pkg_df$lib_size <- sapply(
+      pkg_df$Library,
+      function(x){
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      }
+    )
+  }
   pkg_df
 }
--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -11,7 +11,21 @@ test_that("lib_summary returns packages", {
 })
 
 test_that("lib_summary fails", {
-  expect_error(lib_summary("foo"), "unused argument")
+  expect_error(lib_summary("foo"), "'sizes' must be TRUE or FALSE")
 })
 
 #expect error snapshot_testing
+
+
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  #Datentyp
+  expect_s3_class(res, "data.frame")
+  #Anzahl der Spalten
+  expect_equal(ncol(res), 3)
+  #namen
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$Library, "character")
+  expect_type(res$n_packages, "integer")
+  expect_type(res$lib_size, "double")
+})
